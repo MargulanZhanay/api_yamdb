726f5f265c0b14faa2c884526ce8cdd3a45d7fb4
Margulan Zhanay <mcmrkr@gmail.com>
2023-07-04 21:03:12 +0800

    Add genre title categories models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,91 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        'Название категории',
+        max_length=256
+    )
+    slug = models.SlugField(
+        'Слаг категории',
+        max_length=50,
+        db_index=True,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self) -> str:
+        return self.slug
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        'Название жанра',
+        max_length=256
+    )
+    slug = models.SlugField(
+        'Слаг жанра',
+        max_length=50,
+        db_index=True,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self) -> str:
+        return self.slug
+
+
+class Title(models.Model):
+    name = models.CharField(
+        'Название произведения',
+        max_length=256,
+        db_index=True
+    )
+    year = models.IntegerField(
+        'Год выпуска',
+    )
+    description = models.TextField(
+        'Описание',
+        null=True,
+        blank=True
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        'Жанр произведения',
+        related_name='titles'
+    )
+    category = models.ForeignKey(
+        Category,
+        'Категория произведения',
+        on_delete=models.SET_NULL,
+        related_name='titles'
+    )
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
+
+
+class GenreTitle(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='genres'
+    )
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE,
+        related_name='titles'
+    )
+
+    def __str__(self) -> str:
+        return f'{self.title} {self.genre}'
