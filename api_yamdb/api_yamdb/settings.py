e5e923857cf1489cdc806c8989623197aa244e85
Denis Tretyakov <den.tretyakoff@yandex.ru>
2023-07-09 19:20:41 +0700

    Добавил авторизацию по JWT.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -112,6 +112,13 @@ DEFAULT_FROM_EMAIL = 'admin@admin.ru'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
 
 SIMPLE_JWT = {
